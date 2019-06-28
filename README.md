073b03f2bf526bcdf91f1f46972e8e0a7a240cc5
Moe <muhammad.2712@outlook.com>
2019-06-28 16:32:56 +0100

    Updated log file name

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Observer Log Module
+# Observer Log Magento 1 Module
+
+Log details about admin login and product updates to a log file.
--- src/app/code/local/DigitalOutlet/ObserverLog/Model/Observer.php
+++ src/app/code/local/DigitalOutlet/ObserverLog/Model/Observer.php
@@ -3,14 +3,13 @@ class DigitalOutlet_ObserverLog_Model_Observer
 {
 	public function logProductUpdate(Varien_Event_Observer $observer)
 	{
-		// 
 		if(Mage::helper('observerlog')->isEnabled() == 1 && Mage::helper('observerlog')->isLogProductUpdateEnabled() == 1) {
 			$username = Mage::getSingleton('admin/session')->getUser()->getUsername();
 			$product = $observer->getEvent()->getProduct();
 			$name = $product->getName();
 			$sku = $product->getSku();
 
-			Mage::log("{$username} updated {$sku} | {$name}.", null, 'digitaloutlet_productupdates.log');
+			Mage::log("{$username} updated {$sku} | {$name}.", null, 'observer_log.log');
 		}
 	}
 
@@ -18,7 +17,7 @@ class DigitalOutlet_ObserverLog_Model_Observer
 	{
 		if(Mage::helper('observerlog')->isEnabled() == 1 && Mage::helper('observerlog')->isLogLoginEnabled() == 1) {
 			$username = Mage::getSingleton('admin/session')->getUser()->getUsername();
-			
+
 			if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
 			    $ipAddress = $_SERVER['HTTP_CLIENT_IP'];
 			} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
@@ -27,7 +26,7 @@ class DigitalOutlet_ObserverLog_Model_Observer
 			    $ipAddress = $_SERVER['REMOTE_ADDR'];
 			}
 
-			Mage::log("{$username} has logged in. IP: {$ipAddress}", null, 'digitaloutlet_logins.log');
+			Mage::log("{$username} has logged in. IP: {$ipAddress}", null, 'observer_log.log');
 		}
 	}
 }
